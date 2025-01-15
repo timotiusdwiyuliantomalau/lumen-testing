298e0969e0dcbb9476bd4ddc6b40d12765dc092e
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-01-15 21:45:03 +0700

    Initialize Repo

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,6 +16,7 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $headers = [
+            'Access-Control-Allow-Origin'      => 'http://localhost:3000',
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
